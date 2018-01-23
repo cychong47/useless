b578a867cf2b16039c9a6b1e3822a5563da04a07
cychong47 <cychong@gmail.com>
2018-01-24 01:02:16 +0900

    add run.sh for docker

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -d --name="pyftpd" -p 65500-65520:65500-65520 -v /Users/cychong/work/useless/pyftpd.yaml:/etc/pyftpd.yaml -v /Users/cychong/Dropbox/incoming/:/pyftpd/incoming -v /Users/cychong/tmp/:/pyftpd/log -e PYFTPD_PORT=65500 -e PYFTPD_PASSIVE_PORT=65501-65520 cychong/pyftpd
